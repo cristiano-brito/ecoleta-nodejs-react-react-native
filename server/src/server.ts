3667a16b4283e8bba34d5131b610a1e115320029
Cristiano de Andrade <cristianocomputacao@hotmail.com>
2020-06-02 23:25:43 -0300

    Aula2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,67 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(routes);
 
-//Rota: Endereço completo da requisição
-//Recurso: Qual entidade estamos acessando do sistema
-
-//GET: Buscar uma ou mais informações do back-end
-//POST: Cria uma nova informação no back-end
-//PUT: Atualiza uma informação existente no back-end
-//DELETE: Remove uma informação do back-end
-
-//Post http://localhost:3333/users = Criar um usuário
-//GET http://localhost:3333/users = Listar usuários
-//GET http://localhost:3333/users/5 = Buscar dados do usuário com ID 5
-
-//Request Param: Parâmetros que vem na própria rota que identificam um recurso
-//Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação
-//Request Body: Parâmetros para criação/atualização de informações
-
-//SELECT * FROM users WHERE name = 'Diego'
-//knex('users').where('name', 'Diego').select('*')
-
-const users = [
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel',
-    'Gustavo',
-    'Mauro'
-];
-
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
-
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    console.log(data);
-
-    const user = {
-        name: data.name,
-        email: data.email
-    };
-
-   return response.json(user);
-
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
